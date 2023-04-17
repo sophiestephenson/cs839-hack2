1156a3730385ccbccd89d7df078c824b9c4b2873
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2023-04-17 12:26:40 -0500

    still does the thing, just saving my work

--- hack4/hack4.ino
+++ hack4/hack4.ino
@@ -5,16 +5,18 @@
 #include "AudioTools.h"
 #include "SPIFFS.h"
 #include "arduinosecrets.h"
+#include "WAVFileWriter.h"
 
 I2SStream in;  // Access I2S as a stream
-File audioFile;  //empty audio file object
 
 const char* ssid = WIFI_SSID;   //  your network SSID (name)
 const char* pass = WIFI_PASS;  // your network password
 
-const char* filename = "/audiofile.wav";
-
-void setupWifi() {
+// Arduino Setup
+void setup() {
+  Serial.begin(115200);
+  
+  // Set up wifi
   Serial.print("Connecting to Wifi...");
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
@@ -23,50 +25,27 @@ void setupWifi() {
   } 
   Serial.println();
   Serial.println("Connected to Wifi");
-}
-
-void sendfile() {
-  Serial.println("Attempting to send file...");
-
-  if ((WiFi.status() == WL_CONNECTED)) {    
-    audioFile = SPIFFS.open(filename);
-
-    WiFiClientSecure *m_wifi_client = new WiFiClientSecure();
-    m_wifi_client->connect("api.wit.ai", 443);
-    m_wifi_client->println("POST /speech?v=20200927 HTTP/1.1");
-    m_wifi_client->println("host: api.wit.ai");
-    m_wifi_client->println("authorization: Bearer 6QVLPGAHBAV2AL55FZ4PDIXI2BYDLADR");
-    m_wifi_client->println("content-type: audio/wav;");
-    m_wifi_client->print("content-length: ");
-    m_wifi_client->println(audioFile.size());
-    m_wifi_client->println();
-    m_wifi_client->write(audioFile);
-    m_wifi_client->println();
-
-    audioFile.close();
-
-    Serial.println("File sent.");
-  }
-}
-
-// Arduino Setup
-void setup() {
-  Serial.begin(115200);
-  setupWifi();
-
+  
   SPIFFS.begin();
+
+  //Serial.println("starting out stream...");
+  //WAVEncoder *wavEncoder = new WAVEncoder();
+  //EncodedAudioStream out(&audioFile, wavEncoder);  // Access audioFile as a stream
+  //StreamCopy copier(out, in); 
 
   Serial.println("starting I2S...");
   auto config = in.defaultConfig(RX_MODE);
   config.i2s_format = I2S_STD_FORMAT;  // if quality is bad change to I2S_LSB_FORMAT #23
-  config.sample_rate = 44100;
-  config.channels = 1;
-  config.bits_per_sample = 32;
+  config.sample_rate = 16000;
+  config.channels = 2;
+  config.bits_per_sample = 16;
   in.begin(config);
   Serial.println("I2S started");
 
-  Serial.println("starting out stream...");
-  EncodedAudioStream out(&audioFile, new WAVEncoder());  // Access audioFile as a stream
+  SPIFFS.remove("/audiofile.wav");
+
+  File audioFile = SPIFFS.open("/audiofile.wav", FILE_WRITE); 
+  EncodedAudioStream out(&audioFile, new WAVEncoder());
   auto cfg_out = out.defaultConfig();
   cfg_out.bits_per_sample = 32;
   cfg_out.channels = 1;
@@ -75,24 +54,38 @@ void setup() {
 
   StreamCopy copier(out, in);  
 
-  SPIFFS.remove(filename);
-  audioFile = SPIFFS.open(filename, FILE_WRITE);  
-
   Serial.println("RECORDING");
   long starttime = millis();
   while (starttime + 2000 > millis())  // loop end after 2 sec
   {
     copier.copy();  // Copy sound from I2S to the audio file
   }
-  audioFile.close();  // Closes the file after 5 seconds
+  copier.end();
+  in.end();
+  out.end();
   Serial.println("Stopped recording");
 
-  // get file size
-  audioFile = SPIFFS.open(filename, FILE_READ);
-  size_t fileSize1 = audioFile.size();
-  Serial.println("FILE SZE:" + String(fileSize1));
+  // Send the file
+  Serial.println("Attempting to send file...");
+
+  if ((WiFi.status() == WL_CONNECTED)) {    
+    //audioFile = SPIFFS.open("/audiofile.wav", FILE_READ);
+
+    WiFiClientSecure m_wifi_client; // = new WiFiClientSecure();
+    m_wifi_client.connect("api.wit.ai", 443);
+    m_wifi_client.println("POST /speech?v=20200927 HTTP/1.1");
+    m_wifi_client.println("host: api.wit.ai");
+    m_wifi_client.println("authorization: Bearer 6QVLPGAHBAV2AL55FZ4PDIXI2BYDLADR");
+    m_wifi_client.println("content-type: audio/wav;");
+    m_wifi_client.print("content-length: ");
+    m_wifi_client.println(audioFile.size());
+    m_wifi_client.println();
+    m_wifi_client.write(audioFile);
+    m_wifi_client.println();
+    m_wifi_client.stop();
+    //audioFile.close();
+  }
   audioFile.close();
-  sendfile();
 }
 
 void loop() {
