329e472629e72a6c2e930ad578485b2955dbcd74
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2023-04-17 10:11:56 -0500

    clean up, still getting bad heap

--- hack4/hack4.ino
+++ hack4/hack4.ino
@@ -1,51 +1,35 @@
 #include <WiFiClientSecure.h>
-#include <esp_crt_bundle.h>
-#include <ssl_client.h>
+#include <WiFi.h>
 
-#include <SPI.h>
 #include "Arduino.h"
 #include "AudioTools.h"
 #include "SPIFFS.h"
-
-#define PIN_AUDIO_KIT_SD_CARD_CS 5
-#define PIN_AUDIO_KIT_SD_CARD_MISO 19
-#define PIN_AUDIO_KIT_SD_CARD_MOSI 23
-#define PIN_AUDIO_KIT_SD_CARD_CLK 18
-const uint16_t sample_rate = 44100;
-
-bool recording = false;
-unsigned long runtime;
+#include "arduinosecrets.h"
 
 I2SStream in;  // Access I2S as a stream
-//AnalogAudioStream in;
 File audioFile;  //empty audio file object
 
-char ssid[] = "TP-Link_506D";   //  your network SSID (name)
-char pass[] = "56709179";  // your network password
-int status = WL_IDLE_STATUS;
-// Initialize the client library
-WiFiClientSecure client;
+const char* ssid = WIFI_SSID;   //  your network SSID (name)
+const char* pass = WIFI_PASS;  // your network password
+
+const char* filename = "/audiofile.wav";
 
 void setupWifi() {
-  Serial.println("Attempting to connect to WPA network...");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
-
-  status = WiFi.begin(ssid, pass);
-  if (status != WL_CONNECTED) {
+  Serial.print("Connecting to Wifi...");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    // wait 1 second for re-trying
     delay(1000);
-  } else {
-    Serial.println("Connected to wifi");
-  }
+  } 
+  Serial.println();
+  Serial.println("Connected to Wifi");
 }
 
 void sendfile() {
-  const String url = "http://maker.ifttt.com/trigger/sensor_status/with/key/bc3z7e6-EAtEmsTx3FrdgJ";
+  Serial.println("Attempting to send file...");
 
-  if ((WiFi.status() == WL_CONNECTED)) {
-    File file = SPIFFS.open("/AudioFile01.wav");
+  if ((WiFi.status() == WL_CONNECTED)) {    
+    audioFile = SPIFFS.open(filename);
 
     WiFiClientSecure *m_wifi_client = new WiFiClientSecure();
     m_wifi_client->connect("api.wit.ai", 443);
@@ -54,38 +38,23 @@ void sendfile() {
     m_wifi_client->println("authorization: Bearer 6QVLPGAHBAV2AL55FZ4PDIXI2BYDLADR");
     m_wifi_client->println("content-type: audio/wav;");
     m_wifi_client->print("content-length: ");
-    m_wifi_client->println(file.size());
+    m_wifi_client->println(audioFile.size());
     m_wifi_client->println();
-    m_wifi_client->write(file);
+    m_wifi_client->write(audioFile);
     m_wifi_client->println();
+
+    audioFile.close();
+
+    Serial.println("File sent.");
   }
 }
 
 // Arduino Setup
-void setup(void) {
+void setup() {
   Serial.begin(115200);
   setupWifi();
 
-  AudioLogger::instance().begin(Serial, AudioLogger::Debug);  // AudioLogger set to Debug mode
-
-  Serial.println("SERIAL STARTED");
-  SPI.begin(PIN_AUDIO_KIT_SD_CARD_CLK, PIN_AUDIO_KIT_SD_CARD_MISO, PIN_AUDIO_KIT_SD_CARD_MOSI, PIN_AUDIO_KIT_SD_CARD_CS);
-
-  if (!SPIFFS.begin(PIN_AUDIO_KIT_SD_CARD_CS)) {
-
-    Serial.println("SPIFFS FAILED");
-  }
-
-  // configure I2S stream
-  Serial.println("starting I2S...");
-
-  EncodedAudioStream out(&audioFile, new WAVEncoder());  // Access audioFile as a stream
-  StreamCopy copier(out, in);                            // copies sound from i2s to audio file stream
-
-  // RX automatically uses port 0 with pin GPIO34
-  // auto cfgRx = in.defaultConfig(RX_MODE);
-  // cfgRx.sample_rate = sample_rate;
-  // in.begin(cfgRx);
+  SPIFFS.begin();
 
   Serial.println("starting I2S...");
   auto config = in.defaultConfig(RX_MODE);
@@ -96,38 +65,37 @@ void setup(void) {
   in.begin(config);
   Serial.println("I2S started");
 
-  // Configure out stream
+  Serial.println("starting out stream...");
+  EncodedAudioStream out(&audioFile, new WAVEncoder());  // Access audioFile as a stream
   auto cfg_out = out.defaultConfig();
   cfg_out.bits_per_sample = 32;
   cfg_out.channels = 1;
   cfg_out.sample_rate = 44100;
   out.begin(cfg_out);
-  //copier.setCheckAvailableForWrite(false);
 
-  SPIFFS.remove("/AudioFile01.wav");
-  Serial.println("FILE open");
-  File file = SPIFFS.open("/AudioFile01.wav", FILE_READ);
-  size_t fileSize = file.size();
-  Serial.println("FILE SZE:" + String(fileSize));
-  file.close();
-  audioFile = SPIFFS.open("/AudioFile01.wav", FILE_WRITE);  // Open a new file on the SD card for writing to
-  copier.begin(out, in);                                    // Configure copier input and output
+  StreamCopy copier(out, in);  
+
+  SPIFFS.remove(filename);
+  audioFile = SPIFFS.open(filename, FILE_WRITE);  
+
   Serial.println("RECORDING");
-  while (runtime < 2000)  // loop end after 30 sec
+  long starttime = millis();
+  while (starttime + 2000 > millis())  // loop end after 2 sec
   {
-    runtime = millis();
     copier.copy();  // Copy sound from I2S to the audio file
   }
   audioFile.close();  // Closes the file after 5 seconds
-  Serial.println("FILE CLOSED");
-  audioFile = SPIFFS.open("/AudioFile01.wav", FILE_READ);
+  Serial.println("Stopped recording");
+
+  // get file size
+  audioFile = SPIFFS.open(filename, FILE_READ);
   size_t fileSize1 = audioFile.size();
   Serial.println("FILE SZE:" + String(fileSize1));
   audioFile.close();
-  Serial.println("FILE END");
-  delay(5000);
-
   sendfile();
 }
+
 void loop() {
+  Serial.println("Got to loop");
+  delay(2000);
 }
