6454a945818659ea2226507e2e971b0d0e9aabb5
Sophie Stephenson <38193839+sophiestephenson@users.noreply.github.com>
2023-04-18 09:01:25 -0500

    Revert to Hack 2

--- hack2.ino
+++ hack2.ino
@@ -1,45 +1,28 @@
 #define trigPin 13 // define TrigPin
 #define echoPin 14 // define EchoPin.
 #define buzzerPin 12 // define BuzzerPin.
-#define recvPin 15 // define ReceiverPin for IR remote.
 #define MAX_DISTANCE 200 // Maximum sensor distance is rated at 400-500cm.
 
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino.h>
-#include <IRremoteESP8266.h>
-#include <IRrecv.h>
-#include <IRutils.h>
-#include <MPU6050_tockn.h>
-#include <Wire.h>
-
-// CONFIG
-const bool doIFTTT = false; // notify the owner?
-const int period = 200; // each window is 1/5 of a second
-const int ultrasonicMin = 100; // 100 ms min between ultrasonic readings
-const unsigned long remoteTrigger = 0xFF30CF; // the 1 button
 
 // STATE
 const float DANGER_DIST = 25.0;
 bool inDangerZone = false;
-
-// DATA COLLECTION
-long timer = 0;
-long ultrasonicTimer = 0;
-float sumDistInPeriod = 0;
-int numDistReadings = 0;
-bool stoppedInPeriod = false;
-bool startedInPeriod = false;
-bool buzzerOn = false;
-
-// IR STUFF
-IRrecv irrecv(recvPin); // Create a class object used to receive class
-decode_results results; // Create a decoding results class object
   
 // SONAR STUFF
 float timeOut = MAX_DISTANCE * 60;
 int soundVelocity = 340; // define sound speed=340m/s
 
+// WEB STUFF
+const char* ssid = "Galaxy";
+const char* password = "";
+const int    HTTP_PORT   = 80;
+const String HTTP_METHOD = "GET"; // or "POST"
+const char   HOST_NAME[] = "google.com"; // hostname of web server:
+const String PATH_NAME   = "";
+
+const String IFTTT_URL = "http://maker.ifttt.com/trigger/sensor_status/with/key/bc3z7e6-EAtEmsTx3FrdgJ";
 
 void setup() {
   Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results.
@@ -47,129 +30,39 @@ void setup() {
   pinMode(buzzerPin,OUTPUT); // Set Buzzer pin to output mode
   pinMode(echoPin,INPUT); // set echoPin to input mode
 
-  irrecv.enableIRIn(); // Start the IR receiver
-
-  if (doIFTTT) {
-    setUpWifi();
-  }
-}
-
-void loop() {
-  
-  // avoid too quick ultrasonic measurements
-  if (millis() - ultrasonicTimer > ultrasonicMin) {
-    // Data capture
-    float catDist = getSonar();
-    sumDistInPeriod += catDist;
-    numDistReadings++;
-
-    ultrasonicTimer = millis();
-  }
-
-  String remote = remoteTriggered();
-  if (remote == "start") {
-    startedInPeriod = true;
-  }
-  else if (remote == "stop") {
-    stoppedInPeriod = true;
-  }
-
-  // Send data every period
-  if (millis() - timer > period) {
-    updateDangerZone();
-    handleBuzzer();
-
-    // Calculate average over the window & send data
-    float avgDist = getAvg(sumDistInPeriod, numDistReadings);  
-    sendData(avgDist, 1, startedInPeriod, stoppedInPeriod);
-    
-    // Reset
-    sumDistInPeriod = 0;
-    numDistReadings = 0;
-    startedInPeriod = false;
-    stoppedInPeriod = false;
-
-    timer = millis();
-  }
-}
-
-void updateDangerZone() {
-  // read prediction from the python
-  if(Serial.available() > 0) {
-    int prediction = Serial.parseInt();
-    inDangerZone = (prediction == 1);
-  } 
-  else { 
-    inDangerZone = false;
-  }
-}
-
-void handleBuzzer() {
-    // Buzzer logic
-    if (inDangerZone && !buzzerOn) {
-        startBuzzer();
-        if (doIFTTT) {
-          notifyOwner();
-        }
-        Serial.println("Buzzer on");
-        buzzerOn = true;
-      }
-      
-    else if (!inDangerZone && buzzerOn) {
-      stopBuzzer();
-      Serial.println("Buzzer off");
-      buzzerOn = false;
-    }
-}
-
-void sendData(float avgDist, float avgAcc, bool startedInPeriod, bool stoppedInPeriod) {
-
-  String classification = "-";
-  if (startedInPeriod) {
-    classification = "start";
-  }
-  else if (stoppedInPeriod) {
-    classification = "stop";
-  }
-
-  Serial.print(millis());
-  Serial.print(",");
-  Serial.print(avgDist);
-  Serial.print(",");
-  Serial.println(classification);
-}
-
-void notifyOwner() {
-  const String IFTTT_URL = "http://maker.ifttt.com/trigger/sensor_status/with/key/bc3z7e6-EAtEmsTx3FrdgJ";
-  
-  if ((WiFi.status() == WL_CONNECTED)) {
-    HTTPClient http;
-    http.begin(IFTTT_URL);
-    http.GET();
-    http.end();   
-  }
-}
-
-void setUpWifi() {
-  const char* ssid = "Galaxy";
-  const char* password = "";
-
+  // Set up WiFi
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
+  
   Serial.println("Connected to the WiFi network");
 }
 
-float getAvg(float sumReadings, int numReadings) {
-  if (numReadings > 0) {
-    return sumReadings / numReadings;
-  }
-  return 0;
+void loop() {
+  float catDist = getSonar();
+  printDist(catDist);
+
+  if (catDist != 0) { // Avoid 0 issue
+    
+    if (catDist <= DANGER_DIST && !inDangerZone) {
+      inDangerZone = true;
+      startBuzzer();
+      textOwner();
+    }
+    
+    else if (catDist > DANGER_DIST && inDangerZone) {
+      inDangerZone = false;
+      stopBuzzer();
+    }
+
+  } 
+  
+  delay(100); // Wait 100ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 }
 
-// ------ SENSOR HANDLING --------
 
 void startBuzzer() {
   int x = 0;
@@ -179,8 +72,31 @@ void startBuzzer() {
   tone(buzzerPin, toneVal);
 }
 
+void setBuzzerTone(float catDist) {
+  long x = map(catDist, 0, 25, 360, 0);
+  float sinVal = sin(x * (PI / 180));
+  int toneVal = 2000 + sinVal * 500;
+  tone(buzzerPin, toneVal);
+  Serial.print(toneVal);
+}
+
 void stopBuzzer() {
   ledcDetachPin(buzzerPin);
+}
+
+void textOwner() {
+  if ((WiFi.status() == WL_CONNECTED)) {
+    HTTPClient http;
+    http.begin(IFTTT_URL);
+    http.GET();
+    http.end();   
+  }
+}
+
+void printDist(float distance) {
+  Serial.print("Distance: ");
+  Serial.print(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
 }
 
 float getSonar() {
@@ -193,21 +109,3 @@ float getSonar() {
   distance = (float)pingTime * soundVelocity / 2 / 10000; // calculate the distance according to the time
   return distance; // return the distance value
 }
-
-String remoteTriggered() {
-  unsigned long val = 0;
-
-  if (irrecv.decode(&results)) { // Waiting for decoding
-    val = results.value;
-    irrecv.resume(); // Receive the next value
-  }
-
-  if (val == 0xFF02FD)
-    return "start";
-
-  if (val == 0xFF9867)
-    return "stop";
-
-  return "";
-}
-
